2a2214b4708f6cb93d5a11967a1a3226ba0e5987
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2023-01-17 19:47:35 -0500

    Button, ClearButton and Screen components

---
+++ src/Components/Button.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Stylesheets/Button.css";
+
+function Button(props) {
+    const isOperator = (value) => {
+        return isNaN(value) && (value!== '.') && (value !== '=');
+    }
+    
+    return (
+        <div className={`button-container${isOperator(props.children) ? ' operator' : ''}`}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Button;
---
+++ src/Components/ClearBtn.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ClearBtn = (props) => (
+    <div>
+        {props.children}
+    </div>
+);
+
+export default ClearBtn;
